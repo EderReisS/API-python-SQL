0ff82bfb6cb3980526c5bd604c981e7525d98899
EderReisS <ederdt.3@gmail.com>
2021-12-13 17:26:14 -0300

    route version #1

--- api.py
+++ api.py
@@ -153,25 +153,25 @@ def delete_user(user_id):
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/api/users', methods=['GET'])
+@app.route('/api/v1/users', methods=['GET'])
 def api_get_users():
     return jsonify(get_users())
 
-@app.route('/api/users/<user_id>', methods=['GET'])
+@app.route('/api/v1/users/<user_id>', methods=['GET'])
 def api_get_user(user_id):
     return jsonify(get_user_by_id(user_id))
 
-@app.route('/api/users/add',  methods = ['POST'])
+@app.route('/api/v1/users/add',  methods = ['POST'])
 def api_add_user():
     user = request.get_json()
     return jsonify(insert_user(user))
 
-@app.route('/api/users/update',  methods = ['PUT'])
+@app.route('/api/v1/users/update',  methods = ['PUT'])
 def api_update_user():
     user = request.get_json()
     return jsonify(update_user(user))
 
-@app.route('/api/users/delete/<user_id>',  methods = ['DELETE'])
+@app.route('/api/v1/users/delete/<user_id>',  methods = ['DELETE'])
 def api_delete_user(user_id):
     return jsonify(delete_user(user_id))
 
