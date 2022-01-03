6d7c3f52e5fbccea2b6d324ceec9a197016c6083
EderReisS <ederdt.3@gmail.com>
2022-01-03 16:04:58 -0300

    database feature #1

--- database/query/query_imc.py
+++ database/query/query_imc.py
@@ -1,11 +1,30 @@
 class QueryIMC:
-    CREATE_TABLE_USERS: str = """
-    CREATE TABLE IF NOT EXISTS users (
-                user_id INTEGER PRIMARY KEY NOT NULL,
-                name TEXT NOT NULL,
-                email TEXT NOT NULL,
-                phone TEXT NOT NULL,
-                address TEXT NOT NULL,
-                country TEXT NOT NULL
+    CREATE_TABLE_IMC: str = """
+    CREATE TABLE IF NOT EXISTS imc (
+                imc_id INTEGER PRIMARY KEY NOT NULL,
+                birthday TEXT NOT NULL,
+                height REAL NOT NULL,
+                weight REAL NOT NULL,
+                imc_value REAL NOT NULL,
+                user INTERGER NOT NULL,
+                FOREIGN KEY user REFERENCES users(user_id)  
             );
     """
+
+    INSERT_IMC: str = """
+    INSERT INTO imc (birthday, height, weight, imc_value, user) VALUES (?, ?, ?, ?, ?)
+    """
+    GET_IMC_BY_ID: str = """
+    SELECT * FROM imc WHERE user_id = ?
+    """
+    GET_IMCS: str = """
+    SELECT * FROM imc
+    """
+    UPDATE_IMC_BY_ID: str = """
+    UPDATE imc SET birthday = ?, height = ?, weight = ?, imc_value = ?, user = ? WHERE imc_id =?
+    """
+    DELETE_IMC_BY_ID: str = """
+    DELETE from imc WHERE imc_id = ?
+    """
+
+query_imc = QueryIMC()
